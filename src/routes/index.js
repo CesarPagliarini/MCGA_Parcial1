6d2983684b77c3db0b1cee365a29561559563f20
CesarPagliarini <capagliarini@gmail.com>
2021-10-12 11:38:14 -0300

    Agregado de rutas de clientes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,10 @@
 const express = require('express')
 
-
+const clientes = require('./clientes')
 
 const router = express.Router();
 
+router.use('/clientes', clientes);
 
 
 module.exports = router
