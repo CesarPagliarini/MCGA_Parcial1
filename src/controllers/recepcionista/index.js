2649e97860729e79f3628b8cba535f5457775ca6
Alejandro Vozella <alejandrovozella@gmail.com>
2021-10-13 09:20:09 -0300

    Metodo getRecepcionistaByID

--- src/controllers/recepcionista/index.js
+++ src/controllers/recepcionista/index.js
@@ -15,6 +15,30 @@ const getRecepcionista=async(req,res)=>{ try {
 
 };
 
+const getRecepcionistaById= async (req,res)=>{
+    try {
+        const response =await recepcionistaEsquema.findOne({_id: req.params.recepcionistaId});
+
+        if(!response || response.length===0){
+            return res.status(404).json({
+                error:true,
+                message:"Recepcionista no Encontrado"
+            })
+        }
+        return res.status(200).json({
+            data:response,
+            error:false,
+        })
+        
+    } catch (error) {
+        return res.status(400).json({
+            error:true,
+            message:error
+        })
+        
+    }
+};
+
 const addRecepcionista=async(req,res)=>{
     try {
         const recepcionista = new recepcionistaEsquema(req.body);
@@ -31,9 +55,34 @@ const addRecepcionista=async(req,res)=>{
         })
     }
 };
+
+const deleteRecepcionistaById=async(req,res)=>{
+try {
+    const response=await recepcionistaEsquema.findOneAndRemove({_id: req.body.recepcionistaId});
+    if (!response || response.length===0){
+        return res.status(404).json({
+            error:true,
+            message:"Recepcionista no Encontrado"
+        })
+    }
+    return res.status(202).json ({
+        data:response,
+        error:false
+    })
+    
+} catch (error) {
+    return res.status(400).json({
+        error:true,
+        message:error
+    })
+    
+}
+};
    
 
 module.exports={
     getRecepcionista,
-    addRecepcionista
+    getRecepcionistaById,
+    addRecepcionista,
+    deleteRecepcionistaById
 }
--- src/routes/recepcionista/index.js
+++ src/routes/recepcionista/index.js
@@ -4,7 +4,12 @@ const controller=require('../../controllers/recepcionista');
 const router =express.Router();
 
 router.route('/').get(controller.getRecepcionista);
+
+router.route('/:recepcionistaId').get(controller.getRecepcionistaById);
+
 router.route('/').post(controller.addRecepcionista);
+
+router.route('/:recepcionistaId').delete(controller.deleteRecepcionistaById);
 
 
 module.exports = router
