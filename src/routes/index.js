039a58f4380b092e4cba9bfc37f1788663fb4ac0
CesarPagliarini <capagliarini@gmail.com>
2021-10-14 18:07:51 -0300

    intento Heroku

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,11 @@
 const express = require('express')
 
 const clientes = require('./clientes')
-const cabañas = require('./cabañas')
+const cabañas = require('./cabanas')
 
 const router = express.Router();
 
 router.use('/clientes', clientes);
-router.use('/cabañas', cabañas);
+router.use('/cabanas', cabañas);
 
 module.exports = router
